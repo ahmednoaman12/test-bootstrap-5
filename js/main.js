a1aa5d83a189d892003673492eb4ed2659923fa9
ahmednoaman12 <ahmednoamanhassan@gmail.com>
2021-09-16 18:09:23 +0200

    first update

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+// start offcanvas
+(function () {
+  'use strict'
+  
+  document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  })
+
+})()
+
+// end offcanvas
