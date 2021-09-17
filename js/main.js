4bee258593c420c5613071eaa5f85a3d220b126c
ahmednoaman12 <ahmednoamanhassan@gmail.com>
2021-09-17 14:59:03 +0200

    first project bootstarp 5 with canvas

--- js/main.js
+++ js/main.js
@@ -7,6 +7,15 @@
     document.querySelector('.offcanvas-collapse').classList.toggle('open')
   })
 
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWhtZWRub2FtYW5oYXNzYW4iLCJhIjoiY2t0bm51NGV1MDRxdzJ6b3h1aXVhcHZwdiJ9.FSO6p8U1FGny7jmWNX1M-Q';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center : [29.939648,31.220764],
+zoom:13,
+});
 })()
 
 // end offcanvas
